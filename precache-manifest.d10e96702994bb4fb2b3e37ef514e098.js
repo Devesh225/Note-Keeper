dcd4427a564f5a3d2368d01874d2e58e45f407ac
unknown <deveshtulshyan@gmail.com>
2020-08-13 22:40:30 +0530

    Updates

---
+++ precache-manifest.d10e96702994bb4fb2b3e37ef514e098.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "58eb395731f26b44e003ec1f7e9a3670",
+    "url": "/Note-Keeper/index.html"
+  },
+  {
+    "revision": "4bf0a23418cb33cf943d",
+    "url": "/Note-Keeper/static/js/2.c07f458e.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/Note-Keeper/static/js/2.c07f458e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "66e57426b4f3017abf24",
+    "url": "/Note-Keeper/static/js/main.c9a61188.chunk.js"
+  },
+  {
+    "revision": "13d1fafbda27a68a5558",
+    "url": "/Note-Keeper/static/js/runtime-main.6cfc2f38.js"
+  }
+]);
